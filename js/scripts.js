4fc0bdd77759acadc78fcf11579bf97bca08fd8a
vladliena <sotnikovavladlena@gmail.com>
2022-07-07 16:57:34 +0300

    test

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        autoplay: true,
+        items:1,
+        autoplaySpeed: 1200,
+        loop:true,
+        margin:0,
+        nav:false,
+        dots:false,
+        navText: [ '', ' ' ],
+    });
+});
